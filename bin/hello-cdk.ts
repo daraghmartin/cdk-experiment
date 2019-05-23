d40d947404aaf8244ce1715467412944ca807c6d
Daragh Martin <daragh@daragh.net>
2019-05-23 20:57:59 +0000

    add first interfaces

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -1,18 +1,22 @@
 import cdk = require('@aws-cdk/cdk');
 import s3 = require('@aws-cdk/aws-s3');
 import configurator = require('../lib/configurator');
+import { ProjectConfig } from '../lib/configurator';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // let a :string = 'hi';
-    new configurator.Config({cwd: process.cwd()});
-
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true
-    });
-  }
+    config = new configurator.Config({cwd: process.cwd()});
+  
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        // let a :string = 'hi';
+    
+        let projectConfig: ProjectConfig = this.config.getProjectConfig();
+        console.log(projectConfig);
+    
+        new s3.Bucket(this, 'MyFirstBucket', {
+            versioned: true
+        });
+    }
 }
 
 const app = new cdk.App();
--- lib/configurator.ts
+++ lib/configurator.ts
@@ -2,43 +2,51 @@ import yaml = require('js-yaml');
 var path = require('path');
 import * as fs from 'fs';
 
+export interface ProjectConfig {
+    sourceRegion: string;
+    sourceBucket?: string;
+    applicationName?: string;
+    projectName?: string;
+    version?: string;
+}
+
+export interface Options {
+    cwd?: string;
+}
+
 export class Config {
     name:string;
     config:any;
     options:any;
-    
-    constructor(options :any) {
+
+    constructor(options :Options) {
+        // TODO: default options, e.g. cwd = cwd: process.cwd()
         this.options = options;
-        console.log('it\'s alive');
-        console.log(this.getConfig());
+        this.getProjectConfig();
     }
     
-  getConfig():object {
-    //   const config = yaml.safeLoad(fs.readFileSync(`${this.options.cwd}/config/test.yml`, 'utf8'));
-    //   const config2 = yaml.safeLoad(fs.readFileSync(`${this.options.cwd}/config/test2.yml`, 'utf8'));
-    //   const object3 = {...config, ...config2 }
-      //return { alpha: 'beta' };
-      return this.readFilesSync(`${this.options.cwd}/config/`);
-  }
+    getConfig():object {
+        this.config = this.readFilesSync(`${this.options.cwd}/config/`);
+        return this.config;
+    }
+  
+    getProjectConfig(): ProjectConfig {
+        let ProjectConfig: ProjectConfig = { sourceRegion: 'ap-southeast-2'};
+        // console.log(this.config);
+        return ProjectConfig;
+    }
   
     readFilesSync(dir :string):any {
         let config: any = {};
-    
         // assumption that this is natural sort
+        // because we want a predictable sort
         fs.readdirSync(dir).forEach(filename => {
             const name = path.parse(filename).name;
             const ext = path.parse(filename).ext;
-            const filepath = path.resolve(dir, filename);
-            //const stat = fs.statSync(filepath);
-            //const isFile = stat.isFile();
-            const isYaml = (ext === '.yml');
-    
-            if (isYaml) console.log('is yaml');
-            console.log(name);
-            console.log(ext);
-            console.log(filepath);
-            let temp = yaml.safeLoad(fs.readFileSync(`${this.options.cwd}/config/${name}${ext}`, 'utf8'));
-            config = {...config, ...temp }
+            if (ext === '.yml') {
+              let temp = yaml.safeLoad(fs.readFileSync(`${this.options.cwd}/config/${name}${ext}`, 'utf8'));
+              config = {...config, ...temp }
+            }
         });
     
         return config;
